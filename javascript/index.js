f157734b3da4b74b0a8531db9fbc60a2c502cbec
Rahin Haque <haquerahin743@gmail.com>
2026-03-05 22:29:47 +0600

    Design & Display Dynamic cards

--- javascript/index.js
+++ javascript/index.js
@@ -4,16 +4,57 @@ const loadLessons = () => {
     .then((json) => displayLessons(json.data));
 };
 
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) =>displayLevelWord(data.data));
+};
+
+const displayLevelWord=(words)=>{
+ const wordContainer = document.getElementById("word-container");
+ wordContainer.innerHTML = "";
+
+ words.forEach(word => {
+  console.log(word);
+  
+// level: 1;
+// meaning: "আগ্রহী";
+// pronunciation: "ইগার";
+// word: "Eager";
+  const div = document.createElement("div");
+  div.innerHTML = `
+
+   <div class="bg-white rounded-xl shadow-sm text-center space-y-5 px-5 py-10">
+        <h2 class="font-bold text-4xl">${word.word}</h2>
+        <p class="text-md font-semibold">Meaning /Pronounciation</p>
+
+        <div class="text-2xl">"${word.meaning}/ ${word.pronunciation}"</div>
+
+        <div class="flex justify-between items-center">
+
+          <button class="btn border border-none bg-slate-200 rounded-md p-2 hover:bg-cyan-200"><i class="fa-solid fa-info"></i></button>
+          <button class="btn border border-none bg-slate-200 rounded-md p-2 hover:bg-cyan-200"><i class="fa-solid fa-volume-high"></i></button>
+
+        </div>
+      </div>
+
+  
+  `;
+
+  wordContainer.append(div);
+ }) 
+}
+
 const displayLessons = (lessons) => {
   const levelContainer = document.getElementById("level-container");
   levelContainer.innerHTML = "";
 
   for (let lesson of lessons) {
-    const btn = document.createElement("button");
-    btn.className =
-      "flex items-center gap-2 px-5 py-2.5 bg-white border border-gray-200 rounded-full shadow-sm text-sm font-semibold text-gray-700 cursor-pointer hover:shadow-md hover:border-indigo-300 transition-all duration-200";
-    btn.innerHTML = `<span class="text-indigo-600 text-base"><i class="fa-brands fa-leanpub"></i></span> Lesson -${lesson.level_no}`;
-    levelContainer.appendChild(btn);
+    const div = document.createElement("div");
+    div.innerHTML = `<div onClick="loadLevelWord(${lesson.level_no})" class="flex items-center gap-2 px-5 py-2.5 bg-white border border-gray-200 rounded-full shadow-sm text-sm font-semibold text-gray-700 cursor-pointer hover:shadow-md hover:border-indigo-300 transition-all duration-200"><span class="text-indigo-600 text-base"><i class="fa-brands fa-leanpub"></i></span> Lesson -${lesson.level_no}</div>`;
+
+    levelContainer.appendChild(div);
   }
 };
 
